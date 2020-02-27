7fc1b0336c67fd59c4d575e023e34d3a683b17ce
hirata <hirata@team-lab.com>
2020-02-27 12:51:27 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>BallAlignSimulator</title>
+    <meta name="viewport" data-content="width=device-width,initial-scale=0.7,minimum-scale=0.7,maximum-scale=0.7,user-scalable=no">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="stylesheet" href="css/lib/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/lib/jquery.minicolors.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <script type="text/javascript" src="./js/lib/jquery.min.js"></script>
+    <script type="text/javascript" src="./js/lib/bootstrap.min.js"></script>
+    <script type="text/javascript" src="./js/lib/jquery.minicolors.min.js"></script>
+    <script type="text/javascript" src="./js/align.js"></script>
+</head>
+
+<body>
+</body>
+
+</html>
