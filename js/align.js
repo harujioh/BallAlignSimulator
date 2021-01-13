a0c3be2037bcf2a11738dcd6a7fcb742cf41589a
hirata <hirata@team-lab.com>
2021-01-13 09:48:48 +0900

    パラメータが変更なければ再描画しない

--- js/align.js
+++ js/align.js
@@ -55,6 +55,13 @@ $(document).ready(function () {
         });
     }
 
+    function getParameter() {
+        return $.makeArray($('input')).reduce((a, v) => {
+            a[$(v).attr('id')] = $(v).val();
+            return a;
+        }, {});
+    }
+
     var $form = $('#form');
     function draw() {
         var validation = $('form').is(':valid');
@@ -68,6 +75,10 @@ $(document).ready(function () {
             $form.removeClass('invalid');
         }
 
+        if (JSON.stringify($form.data('parameter')) === JSON.stringify(getParameter())) {
+            return;
+        }
+        $form.data('parameter', getParameter());
         var mmPerPx = parseFloat($('#mm-per-px').val()) || 20;
         var ballRadius = ((parseFloat($('#ball-size').val()) / 2) || 500) / mmPerPx;
         var polySide = parseInt($('#poly-side').val()) || 6;
@@ -213,10 +224,7 @@ $(document).ready(function () {
 
     $('#export').click(function () {
         if (navigator.clipboard) {
-            navigator.clipboard.writeText(JSON.stringify($.makeArray($('input')).reduce((a, v) => {
-                a[$(v).attr('id')] = $(v).val();
-                return a;
-            }, {})));
+            navigator.clipboard.writeText(JSON.stringify(getParameter()));
         }
         return false;
     });
@@ -231,6 +239,8 @@ $(document).ready(function () {
                         $(this).val(obj[key]);
                     }
                 });
+
+                draw();
             })
         }
         return false;
